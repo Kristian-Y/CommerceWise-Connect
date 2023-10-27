de1854fdbee9bc70c56eb653940316f8d8c60dcc
Kristian <krisiyotsov@gmail.com>
2023-10-27 16:59:01 +0300

    Направена main станицата с Register формата

--- proekt/urls.py
+++ proekt/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from proekt import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('usermanagment/', include('proekt.usermanagement.urls'))
 ]
